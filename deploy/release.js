fefa6b2dfa8f64f066cb68b5eb783401154525b2
Peter Yakovlev <gorkiy@gmail.com>
2021-10-31 13:19:56 +0300

    Get commits diff

--- deploy/release.js
+++ deploy/release.js
@@ -1,6 +1,23 @@
 const axios = require('axios');
-const { textChangeRangeNewSpan } = require('typescript');
 const { TRACKER_API, TRACKER_ID, GITHUB_API, REPOSITORY_URL } = require('./config.js');
+
+const createReleaseTicket = async () => {
+    await axios({
+        url: `${TRACKER_API}/v2/issues`,
+        method: 'POST',
+        data: {
+            queue: 'TMP',
+            summary: 'Test task',
+            description: '',
+            type: 'task'
+        },
+        headers: {
+            Authorization: `OAuth AQAAAAAAqu8IAAd44L0wFjF6Z00rm_33Yz7oltM`,
+            OrgId: TRACKER_ID
+        }
+    }).then(res => console.log(res))
+        .catch(e => console.log(e));
+}
 
 const getTags = async () => {
     const tags = [];
@@ -21,32 +38,36 @@ const getTags = async () => {
             url: tag.url,
         }
     }).sort();
-
 }
 
-const createReleaseTicket = async () => {
+const getTagsDiff = async (prevTag, lastTag) => {
+    const result = [];
+
     await axios({
-        url: `${TRACKER_API}/v2/issues`,
-        method: 'POST',
-        data: {
-            queue: 'TMP',
-            summary: 'Test task',
-            description: '',
-            type: 'task'
-        },
-        headers: {
-            Authorization: `OAuth AQAAAAAAqu8IAAd44L0wFjF6Z00rm_33Yz7oltM`,
-            OrgId: TRACKER_ID
-        }
-    }).then(res => console.log(res))
-    .catch(e => console.log(e));
-}
+        method: 'GET',
+        url: `${GITHUB_API}/repos/${REPOSITORY_URL}/compare/${prevTag}...${lastTag}`
+    })
+        .then(res => {
+            result.push(...res.data.commits);
+        })
+        .catch(e => console.log(e));
 
+    return result.map(commit => ({
+        author: commit.commit.author,
+        message: commit.commit.message,
+        url: commit.html_url
+    }));
+}
 
 const createRelease = async () => {
     const tags = await getTags();
-    console.log('tags: ', tags);
-    await createReleaseTicket();
+
+    if (!tags || !tags.length) return;
+    const lastTag = tags[tags.length - 1];
+    const prevTag = tags.length < 2 ? lastTag : tags[tags.length - 2];
+    const diff = await getTagsDiff(prevTag.ref, lastTag.ref);
+
+    console.log('diff: ', diff);
 }
 
 createRelease();
