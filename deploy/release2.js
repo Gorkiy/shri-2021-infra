bcc42a1610e8eea35f5a030dfa61e8c2f79d24bf
Peter Yakovlev <gorkiy@gmail.com>
2021-10-31 16:20:33 +0300

    Complete the first stage

--- deploy/release2.js
+++ deploy/release2.js
@@ -1,25 +1,25 @@
+#!/usr/bin/env node
 const axios = require('axios');
 const { TRACKER_API, TRACKER_ID, GITHUB_API, REPOSITORY_URL } = require('./config.js');
-const OAUTH = 'AQAAAAAAqu8IAAd44L0wFjF6Z00rm_33Yz7oltM';
+const OAUTH = 'AQAAAABX6a9zAAd45o57jeQ8i07jkLtXtUKNSKQ';
 
-const createReleaseTicket = async (id, summary, description) => {
-    console.log([...arguments]);
+const createReleaseTicket = async (uniqueId, summary, description) => {
     await axios({
         url: `${TRACKER_API}/v2/issues`,
         method: 'POST',
         data: {
             queue: 'TMP',
-            summary,
-            description,
+            summary: summary,
+            description: description,
             type: 'task',
-            unique: id
+            unique: uniqueId
         },
         headers: {
             Authorization: `OAuth ${OAUTH}`,
             'X-Org-Id': TRACKER_ID
         }
-    })
-    .catch(e => console.log(e));
+    }).then(res => console.log(res.data))
+        .catch(e => console.log(e));
 }
 
 const updateReleaseTicket = async (ticketId, summary, description) => {
@@ -36,19 +36,19 @@ const updateReleaseTicket = async (ticketId, summary, description) => {
             Authorization: `OAuth ${OAUTH}`,
             'X-Org-Id': TRACKER_ID
         }
-    })
-    .catch(e => console.log(e));
+    }).then(res => console.log(res.data))
+        .catch(e => console.log(e));
 }
 
-const findTicket = async id => {
-    let result = [];
+const findTicket = async (uniqueId) => {
+    let array = [];
     await axios({
         url: `${TRACKER_API}/v2/issues/_search`,
         method: 'POST',
         data: {
             filter: {
                 queue: 'TMP',
-                unique: id
+                unique: uniqueId
             },
         },
         headers: {
@@ -56,26 +56,23 @@ const findTicket = async id => {
             'X-Org-Id': TRACKER_ID
         }
     }).then(res => {
-        result = res.data;
+        array = res.data;
+        //res.data.forEach(item => console.log('item.createdBy: ', item.createdBy));
     })
-    .catch(e => console.log(e));
-
-    return result;
+        .catch(e => console.log(e))
+        ;
+    return array;
 }
 
-const getTagMeta = async url => {
+const getTagMeta = async (url) => {
     let result = {};
-
     await axios({
         method: 'GET',
-        url
+        url: url
     })
-        .then(response => {
-            result = response.data.tagger;
-        })
-        .catch(e => console.log(e));
-
-        return result;
+        .then((response) => { result = response.data.tagger; })
+        .catch(console.error);
+    return result;
 }
 
 const getTags = async () => {
@@ -115,11 +112,36 @@ const getTagsDiff = async (prevTag, lastTag) => {
     return result.map(commit => ({
         author: commit.commit.author,
         message: commit.commit.message,
-        url: commit.html_url
+        url: commit.html_url,
+        //tagCreatedAt: commit.createdAt,
+        //tagAuthor: commit.createdBy.display
     }));
 }
 
-const generateId = (prefix, tag) => `${prefix}/${tag}`;
+const createUniqueName = (prefix, tagName) => {
+    return `${prefix}/${tagName}`;
+}
+
+const createRelease = async () => {
+    const tags = await getTags();
+
+    const lastTagMeta = await getTagMeta(tags[tags.length - 1].object.url);
+
+    if (!tags || !tags.length) return;
+
+    const lastTag = tags[tags.length - 1];
+    const prevTag = tags.length < 2 ? lastTag : tags[tags.length - 2];
+    const diff = await getTagsDiff(prevTag.ref, lastTag.ref);
+
+    const ticketId = createUniqueName(REPOSITORY_URL, lastTag.ver);
+    const tickets = await findTicket(ticketId);
+    if (tickets.length) {
+        updateReleaseTicket(tickets[0].id, `Release artifact ${lastTag.ver}`, createReleaseNotes(lastTagMeta, lastTag.ver, diff));
+    } else {
+        createReleaseTicket(ticketId, `Release artifact ${lastTag.ver}`, createReleaseNotes(lastTagMeta, lastTag.ver, diff));
+    }
+
+}
 
 const createReleaseNotes = (tagMeta, version, diff) => {
     var result = `Version: ${version}\nDate: ${tagMeta.date}\nAuthor: ${tagMeta.name}\n\nRELEASE NOTES:\n`;
@@ -127,25 +149,6 @@ const createReleaseNotes = (tagMeta, version, diff) => {
         result += `[${commit.author.date}] ${commit.message}\n`;
     }
     return result;
-}
-
-const createRelease = async () => {
-    const tags = await getTags();
-    if (!tags || !tags.length) return;
-
-    const lastTag = tags[tags.length - 1];
-    const prevTag = tags.length < 2 ? lastTag : tags[tags.length - 2];
-    const meta = await getTagMeta(lastTag.object.url);
-    const diff = await getTagsDiff(prevTag.ref, lastTag.ref);
-
-    const ticketId = generateId(REPOSITORY_URL, lastTag.ver);
-    const tickets = await findTicket(ticketId);
-
-    if (tickets.length) {
-        updateReleaseTicket(tickets[0].id, `Release artifact ${lastTag.ver}`, createReleaseNotes(meta, lastTag.ver, diff));
-    } else {
-        createReleaseTicket(ticketId, `Release artifact ${lastTag.ver}`, createReleaseNotes(meta, lastTag.ver, diff));
-    }
 }
 
 createRelease();
